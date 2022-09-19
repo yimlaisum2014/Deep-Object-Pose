5ac4175cba036f68c7a7ecaaa842f982a9381c2d
yimlaisum2014 <yimlaisum2014@example.com>
2022-09-19 22:58:28 +0800

    update demo config and fix time_syn problem (1->10)

--- docker_run.sh
+++ docker_run.sh
@@ -1,1 +1,1 @@
-docker run --gpus all  -it --privileged --network=host -it -v "/home/nctuece:/home/" nvidia-dope:noetic-v1 bash
+docker run --gpus all  -it --privileged --network=host -it -v "/home/$USER/catkin_ws:/home/$USER/catkin_ws" yimlaisum2014/dope:gpu-noetic bash
