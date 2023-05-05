f4e3e23875144000ea8f7fac5a9ae1e6ed12edb3
yimlaisum2014 <61962074+yimlaisum2014@users.noreply.github.com>
2023-05-05 19:02:36 +0800

    Update docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -1,1 +1,1 @@
-docker run --gpus all  -it --privileged --network=host -it -v "/home/$USER/catkin_ws:/home/$USER/catkin_ws" yimlaisum2014/dope:gpu-noetic bash
+docker run --gpus all  -it --privileged --network=host -it -v "/home/$USER/Deep-Object-Pose:/home/$USER/Deep-Object-Pose" yimlaisum2014/dope:gpu-noetic bash
